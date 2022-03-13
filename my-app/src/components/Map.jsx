19f3b2c2c670701fa4f4fe083341396fdd1b956b
Jonas A <jonas.amoa@bellevuecollege.edu>
2022-03-13 10:01:13 -0700

    Removed JSON Parsing. Added FB reference to pull data

--- my-app/src/components/Map.jsx
+++ my-app/src/components/Map.jsx
@@ -1,65 +1,115 @@
 import React, { Component,useState,useEffect } from 'react';
-import { Marker, GoogleMap, KmlLayer, LoadScript, Polygon, useGoogleMap, GoogleMapsMarkerClusterer, Polyline, MapContext } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Polygon, InfoWindow } from '@react-google-maps/api';
 import db from './firebase.config';
+import { ref, push, getDatabase, get, child } from 'firebase/database'
 import './Map.css'
 import data from '../states2.json'
+import { collection, doc, query, QuerySnapshot, getDoc } from 'firebase/firestore/lite';
 
-let objs = []
-let objCoords = []
-let finalStateCoords = []
-const countyCoords = []
+function Map()  {
+  const [stateList, setStateList] = useState([])
+  const [stateGeoList, setStateGeoList] = useState([])
+  const [stateLoading, setStateLoading] = useState(true)
+  const mapRef = React.useRef()
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map
+  }, [])
+  const[selected, setSelected] = React.useState(null)
+  
+  //deprecated, no long need stateList
+  // //pulling data from firebase, adding case rate to each doc and options, setting stateList
+  // useEffect(() => {
+  //   const dbRef = ref(getDatabase())
+  //   get(child(dbRef, `states/stateList/`)).then((snapshot => {
+  //     if (snapshot.exists()) {
+  //       stateSnapshot = snapshot.val()
+  //       stateSnapshot.forEach((doc) => {
+  //         let optNumber = ((doc.cases)/(doc.population)*100)
+  //         doc.caseCalculation = optNumber
+  //         // if(optNumber > 20) {
+  //         //   doc.options = highCaseOptions
+  //         // } else if(12 < optNumber < 20) {
+  //         //   doc.options = medCaseOptions
+  //         // } else {
+  //         //   doc.options = lowCaseOptions
+  //         // }
+  //         localStorage.setItem('State Ref', JSON.stringify(stateSnapshot))
+  //       })
+  //       setStateList(stateSnapshot)
+  //       setStateLoading(false)
+  //     } else {
+  //       console.log("No data")
+  //     }
+  //   }))
+  // }, [])
 
-
-//Compiles wanted data from JSON (name/ID/coords) into JS objects and saves in array
-for (let i = 0; i < data.features.length; i++) {
-  let newObj = new Object()
-  newObj.name = data.features[i].properties.name
-  newObj.id = data.features[i].id
-  newObj.coordinates = data.features[i].geometry.coordinates
-  // newObj.coordinates = flatten(newObj.coordinates)
-  objCoords.push(data.features[i].geometry.coordinates)
-  objs.push(newObj)
-}
-
-//loads coordinates into separate arrays, each index is its own state
-function fillObjCoords() {
-  for(let i = 0; i < objs.length; i++) {
-    let stateArray = objs[i].coordinates
-    for(let j = 0; j < stateArray.length; j++) {
-      let miniArray = stateArray[j]
-      for(let k = 0; k < miniArray.length; k++){
-        let thirdArray = miniArray[k]
-        thirdArray.flatten(Infinity)
-        if(thirdArray.length > 2) {
-          for(let l = 0; l < thirdArray.length; l++){
-            let fourthArray = thirdArray[l]
-            let newObj = new Object()
-            newObj.lat = fourthArray[1]
-            newObj.lng = fourthArray[0]
-            objCoords[i].push(newObj)
-          }
-        } else {
-            let newObj = new Object()
-            //let holder = new google.maps.LatLng(thirdArray[0], thirdArray[1])
-            newObj.lat = thirdArray[1]
-            newObj.lng = thirdArray[0]
-            newObj[i].push(newObj)
-            objCoords[i].push(newObj)
-         }
+  useEffect(() => {
+    const dbRef = ref(getDatabase())
+    get(child(dbRef, `stateGeo/fullStates`)).then((snapshot => {
+      if (snapshot.exists()) {
+        stateSnapshot = snapshot.val()
+        stateSnapshot.forEach((doc) => {
+          let optNumber = ((doc.cases)/(doc.population)*100)
+          doc.caseCalculation = optNumber
+          if(optNumber > 20) {
+            doc.options = highCaseOptions
+          } else if(12 < optNumber < 20) {
+            doc.options = medCaseOptions
+          } else {
+            doc.options = lowCaseOptions
+           }
+          localStorage.setItem('State Ref', JSON.stringify(stateSnapshot))
+        })
+        setStateGeoList(stateSnapshot)
         
+      } else {
+        console.log("No data")
       }
-    }
-  }
-}
+    }))
+  }, [])
 
-// getCounties()
-// function getCounties() {
-//   for(let i = 0; i < countyData.length; i++){
-//     let holder = countyData.features[i].geometry
-//     countyCoords.push(holder)
+
+
+
+
+let fullStates = []
+let stateSnapshot = []
+let countyObjs = []
+
+
+
+//deprecated, no longer parsing JSON front end
+// //loads state coordinates into separate arrays, each index is its own state
+// function fillObjCoords() {
+//   for(let i = 0; i < objs.length; i++) {
+//     let stateArray = objs[i].coordinates
+//     for(let j = 0; j < stateArray.length; j++) {
+//       let miniArray = stateArray[j]
+//       for(let k = 0; k < miniArray.length; k++){
+//         let thirdArray = miniArray[k]
+//         thirdArray.flatten(Infinity)
+//         if(thirdArray.length > 2) {
+//           for(let l = 0; l < thirdArray.length; l++){
+//             let fourthArray = thirdArray[l]
+//             let newObj = new Object()
+//             newObj.lat = fourthArray[1]
+//             newObj.lng = fourthArray[0]
+//             objCoords[i].push(newObj)
+//           }
+//         } else {
+//             let newObj = new Object()
+//             newObj.lat = thirdArray[1]
+//             newObj.lng = thirdArray[0]
+//             newObj[i].push(newObj)
+//             objCoords[i].push(newObj)
+//          }
+        
+//       }
+//     }
 //   }
-//   console.log(countyCoords)
 // }
+
+
 
 
 
@@ -72,15 +122,40 @@ const containerStyle = {
   
 };
 
-
 const center = {
   lat: 39.8283,
   lng: -98.5795,
 };
 
-//polygon draw options
-const options = {
-  fillColor: "lightblue",
+
+const highCaseOptions = {
+  fillColor: "red",
+  fillOpacity: .5,
+  strokeColor: "black",
+  strokeOpacity: 1,
+  strokeWeight: 2,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  geodesic: false,
+  zIndex: 1
+}
+
+const medCaseOptions = {
+  fillColor: "yellow",
+  fillOpacity: .5,
+  strokeColor: "black",
+  strokeOpacity: 1,
+  strokeWeight: 2,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  geodesic: false,
+  zIndex: 1
+}
+
+const lowCaseOptions = {
+  fillColor: "green",
   fillOpacity: .5,
   strokeColor: "black",
   strokeOpacity: 1,
@@ -93,103 +168,32 @@ const options = {
 }
 
 
-//maps json coordinates to single level long array of coordinates
-let coordinates = []
-for(let i = 0; i < objs.length; i++){
-  //this is the coordinates of state @ i
-  let crdArray = objs[i].coordinates
-  for(let j = 0; j < crdArray.length; j++){
-    //this is the first array of coordinate arrays @ state @ i
-    let secondArray = crdArray[j]
-    for(let k = 0; k < secondArray.length; k++){
-      let thirdArray = secondArray[k]
-      //check if nested object contains more nested arrays
-      if(thirdArray.length > 2){
-        for(let l = 0; l < thirdArray.length; l++){
-          let fourthArray = thirdArray[l]
-          let newObj = new Object()
-          //let holder = google.maps.LatLng(fourthArray[0], fourthArray[1])
-          newObj.lat = fourthArray[1]
-          newObj.lng = fourthArray[0]
-          coordinates.push(newObj)
-        }
-      //add coordinates to 'coordinates' array
-      } else {
-          let newObj = new Object()
-          //let holder = new google.maps.LatLng(thirdArray[0], thirdArray[1])
-          newObj.lat = thirdArray[1]
-          newObj.lng = thirdArray[0]
-          coordinates.push(newObj)
-      }
+
+  //INFO WINDOW HELPERS
+  function returnCases() {
+    return stateGeoList.find((e) => e.name === selected[0].name).cases
+  }
+
+  function returnDeaths() {
+    return stateGeoList.find((e) => e.name === selected[0].name).deaths
+  }
+
+  function returnVac() {
+    return stateGeoList.find((e) => e.name === selected[0].name).vacRate
+  }
+
+  function getOptions(e) {
+    if(e[0].caseRate > 30) {
+      return highCaseOptions
+    } else if(e[0].caseRate < 20) {
+      return lowCaseOptions
+    } else{
+      return medCaseOptions
     }
   }
-}
 
-console.log(objs)
+ 
 
-let fullStates = []
-function stateParse() {
-  for(let i = 0; i < objs.length; i++) {
-    let newArr = []
-    //this is 0.coordinates.i
-    let coords = objs[i].coordinates
-    if(coords.length === 1) {
-      let holder = coords[0]
-      for(let j = 0; j < holder.length; j++) {
-        let holderTwo = holder[j]
-        let newObj = new Object()
-        newObj.lat = holderTwo[1]
-        newObj.lng = holderTwo[0]
-        newObj.name = objs[i].name
-        newArr.push(newObj)
-      }
-    } 
-    else if(coords.length > 1) {
-      for(let j = 0; j < coords.length; j++){
-        let coordstepone = coords[j]
-        for(let k = 0; k < coordstepone.length; k++){
-          let secondArr = []
-          let coordsteptwo = coordstepone[k]
-          for(let l = 0; l < coordsteptwo.length; l++){
-            let coordstepthree = coordsteptwo[l]
-            let newObj = new Object()
-            newObj.lat = coordstepthree[1]
-            newObj.lng = coordstepthree[0]
-            newObj.name = objs[i].name
-            secondArr.push(newObj)
-          }
-          fullStates.push(secondArr)
-        }
-        
-      }
-    }
-
-    
-    fullStates.push(newArr)
-  }
-}
-
-stateParse()
-console.log(fullStates)
-
-
-
-function Map()  {
-  const [stateList, setStateList] = useState([])
-
-  useEffect(() => {
-    fetchStates();
-  }, [])
-  
-  
-  const fetchStates=async() => {
-    const response = db.document('states')
-    const data = await response.get();
-    data.docs.forEach(item=> {
-      setStateList([...stateList, item.data()])
-    })
-  }
-  
     return (
       <LoadScript
         googleMapsApiKey="AIzaSyBYSwVwuuWe4ZxZpoNuCXWKWfmZXqWh9Lc"
@@ -197,21 +201,43 @@ function Map()  {
         <GoogleMap 
           center={center}
           zoom={4}
+          // onClick={(event) => {
+          //   console.log(event)
+          // }}
+          onload={onMapLoad}
           mapContainerStyle={containerStyle}
         >
-          {fullStates.map((e) => (
-            <Polygon
+          {stateGeoList.map((e, index) => (
+            <Polygon 
+              key = {index}
               paths = {e}
-              options = {options}
+              options = {getOptions(e)}
+              onClick = {() => {
+                setSelected(e[0])
+              }}
               />
           ))}
 
+          {selected ? (<InfoWindow 
+          key = {selected.id}
+          position={{lat:selected[0].lat, lng: selected[0].lng}}
+          onCloseClick={() => {
+            setSelected(null)
+          }}>
+            <div>
+            <h2>{selected[0].name}</h2>
+            <p>All Time Cases: {returnCases()}</p>
+            <p>All Time Deaths: {returnDeaths()}</p>
+            <p>Vaccination Rate: {returnVac()}</p>
+            </div>
+           </InfoWindow>) : null} 
 
 
           
         </GoogleMap>
+        {/* {!showPolygons && <button onclick= {() => setShowPolygons(true)}>Show State Data</button>}
+        {showPolygons && <button onclick= {() => setShowPolygons(false)}>Hide State Data</button>} */}
       </LoadScript>
-
     )
     
 }
