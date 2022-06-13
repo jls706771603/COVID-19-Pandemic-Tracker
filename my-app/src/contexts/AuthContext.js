fcaa8971bc36c97705374efdf397b551b4c77026
jamoa99 <72120020+jamoa99@users.noreply.github.com>
2022-06-12 20:31:08 -0700

    Add files via upload

---
+++ my-app/src/contexts/AuthContext.js
@@ -0,0 +1,94 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import { auth, storage } from '../components/firebase.config'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut,
+    GoogleAuthProvider,
+    signInWithPopup,
+    sendPasswordResetEmail,
+    updateProfile,
+    confirmPasswordReset,
+} from 'firebase/auth'
+
+const AuthContext = createContext({
+    currentUser: null,
+    register: () => Promise,
+    login: () => Promise,
+    logout: () => Promise,
+    signInWithGoogle: () => Promise,
+    forgotPassword: () => Promise,
+    resetPassword: () => Promise,
+})
+
+export const useAuth = () => useContext(AuthContext)
+
+export default function AuthContextProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState(null)
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
+            setCurrentUser(user)
+        })
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
+    function register(email, password) {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    function login(email, password) {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    function signInWithGoogle() {
+        const provider = new GoogleAuthProvider()
+        return signInWithPopup(auth, provider)
+    }
+
+    function forgotPassword(email) {
+        return sendPasswordResetEmail(auth, email, {
+            // KEY POINT
+            url: 'https://pandemic-tracker-1b4e2.web.app/login'
+        })
+    }
+
+    function resetPassword(oobCode, newPassword) {
+        return confirmPasswordReset(auth, oobCode, newPassword)
+    }
+
+    function logout() {
+        return signOut(auth)
+    }
+
+    const value = {
+        currentUser,
+        register,
+        login,
+        logout,
+        signInWithGoogle,
+        forgotPassword,
+        upload,
+        resetPassword,
+    }
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
+
+export async function upload(file, currentUser, setLoading) {
+    const fileRef = ref(storage, currentUser.uid + '.png');
+    console.log(fileRef)
+    setLoading(true)
+    const snapshot = await uploadBytes(fileRef, file);
+
+    const photoURL = await getDownloadURL(fileRef);
+
+    updateProfile(currentUser, {photoURL})
+
+    setLoading(false)
+    alert("Uploaded file")
+}
